8d95aab45a60031367167839667616d38c0df459
Ben Eli <benelashvili@gmail.com>
2022-06-04 15:18:59 +0300

    login and register activities basic design + navigation

--- app/src/main/java/com/example/whatsappandroid/RegisterActivity.java
+++ app/src/main/java/com/example/whatsappandroid/RegisterActivity.java
@@ -1,6 +1,7 @@
 package com.example.whatsappandroid;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +11,8 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        Button btnBack = findViewById(R.id.BackButton);
+        btnBack.setOnClickListener(arg0 -> finish());
     }
 }
