41f078b000e511033c9c20d20dcfbe951cdd773b
saharofe <saharofe01@gmail.com>
2022-06-12 23:39:48 +0300

    added validation for registering and login

--- app/src/main/java/com/example/whatsappandroid/MainActivity.java
+++ app/src/main/java/com/example/whatsappandroid/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.whatsappandroid;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
@@ -20,6 +22,33 @@ public class MainActivity extends AppCompatActivity {
     private UserDao UserDao;
     private List<User> users;
     private UserAPI userAPI;
+
+    private void errorValidation(String Title,String messageError)
+    {
+        AlertDialog.Builder builder
+                = new AlertDialog
+                .Builder(MainActivity.this);
+        builder.setMessage(messageError);
+        builder.setTitle(Title);
+        builder.setCancelable(false);
+        builder
+                .setNegativeButton(
+                        "OK",
+                        new DialogInterface
+                                .OnClickListener() {
+
+                            @Override
+                            public void onClick(DialogInterface dialog,
+                                                int which)
+                            {
+                                dialog.cancel();
+                            }
+                        });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,15 +73,29 @@ public class MainActivity extends AppCompatActivity {
             String username = editTextUsername.getText().toString();
             EditText editTextPassword = findViewById(R.id.editTextPasswordLogin);
             String password = editTextPassword.getText().toString();
-            //users.addAll(UserDao.index());
+            if (username.equals("")){
+                editTextPassword.setText("");
+                errorValidation(getString(R.string.payAttenrion),getString(R.string.UsernameRequired));
+                return;
+            }
+            if(password.equals("")){
+                editTextUsername.setText("");
+                errorValidation(getString(R.string.payAttenrion),getString(R.string.PasswordRequired) );
+                return;
+            }
+
             for(User user : users){
                 if(user.getUserUserName().equals(username)  && user.getPassword().equals(password)){
                     i.putExtra("Username", username);
                     startActivity(i);
-                    break;
+                    return;
                 }
             }
-            //put error message(incorrect password or username)
+            //else, user or password are incorrect
+            editTextPassword.setText("");
+            editTextUsername.setText("");
+            errorValidation(getString(R.string.payAttenrion), getString(R.string.IncorrectInPut));
+
         });
 
     }
--- app/src/main/java/com/example/whatsappandroid/RegisterActivity.java
+++ app/src/main/java/com/example/whatsappandroid/RegisterActivity.java
@@ -1,5 +1,7 @@
 package com.example.whatsappandroid;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
@@ -19,6 +21,43 @@ public class RegisterActivity extends AppCompatActivity {
     private UserDao UserDao;
     private List<User> users;
     private UserAPI userAPI;
+    private EditText editTextUsername;
+    private EditText editTextNickName;
+    private EditText editTextPasswordCheck;
+    private EditText editTextPassword;
+
+    private void cleanAllFeilds(){
+        editTextUsername.setText("");
+        editTextNickName.setText("");
+        editTextPasswordCheck.setText("");
+        editTextPassword.setText("");
+    }
+
+    private void errorValidation(String Title,String messageError)
+    {
+        AlertDialog.Builder builder
+                = new AlertDialog
+                .Builder(RegisterActivity.this);
+        builder.setMessage(messageError);
+        builder.setTitle(Title);
+        builder.setCancelable(false);
+        builder
+                .setNegativeButton(
+                        "OK",
+                        new DialogInterface
+                                .OnClickListener() {
+
+                            @Override
+                            public void onClick(DialogInterface dialog,
+                                                int which)
+                            {
+                                dialog.cancel();
+                            }
+                        });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,19 +74,57 @@ public class RegisterActivity extends AppCompatActivity {
         Button btnLogin = findViewById(R.id.RegisterButton);
         btnLogin.setOnClickListener(v -> {
 
-            EditText editTextUsername = (EditText) findViewById(R.id.editTextUsername);
+            editTextUsername = (EditText) findViewById(R.id.editTextUsername);
             String username = editTextUsername.getText().toString();
 
-            EditText editTextNickName = (EditText) findViewById(R.id.editTextNickname);
+            editTextNickName = (EditText) findViewById(R.id.editTextNickname);
             String nickName = editTextNickName.getText().toString();
 
-            EditText editTextPasswordCheck = (EditText) findViewById(R.id.editTextPasswordCheck);
+            editTextPasswordCheck = (EditText) findViewById(R.id.editTextPasswordCheck);
             String passwordCheck = editTextPasswordCheck.getText().toString();
 
-            EditText editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+            editTextPassword = (EditText) findViewById(R.id.editTextPassword);
             String password = editTextPassword.getText().toString();
 
             // if everything is valid
+            if(username.equals("")){
+               cleanAllFeilds();
+               errorValidation(getString(R.string.payAttenrion),getString(R.string.UsernameRequired));
+                return;
+            }
+            if(nickName.equals("")){
+                cleanAllFeilds();
+                errorValidation(getString(R.string.payAttenrion),getString(R.string.NicknameRequired));
+                return;
+            }
+            if(password.equals("")){
+                cleanAllFeilds();
+                errorValidation(getString(R.string.payAttenrion),getString(R.string.PasswordRequired));
+                return;
+            }
+            if(passwordCheck.equals("")){
+                cleanAllFeilds();
+                errorValidation(getString(R.string.payAttenrion),getString(R.string.ConfirmPasswordReqires));
+                return;
+            }
+            if(username.length() < 3){
+                cleanAllFeilds();
+                errorValidation(getString(R.string.payAttenrion),getString(R.string.InvalidUsername));
+                return;
+            }
+
+            if(!password.matches("^(?=.*[0-9])(?=.*[A-Z])[a-zA-Z0-9!@#$%^&*]{6,20}$")){
+                cleanAllFeilds();
+                errorValidation(getString(R.string.payAttenrion), getString(R.string.PasswordInvalid));
+                return;
+            }
+
+            if(!password.equals(passwordCheck)){
+                cleanAllFeilds();
+                errorValidation(getString(R.string.payAttenrion),getString(R.string.PasswordsNotEqual));
+                return;
+            }
+
             User newUser = new User(username, nickName, password, "");
             UserDao.insert(newUser);
             userAPI.post(newUser);
