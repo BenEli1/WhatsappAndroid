dff62b355a7771c35338c92d99f1992e0a931bb9
Ben <benelashvlili@gmail.com>
2022-06-14 19:16:47 +0300

    added on message received and createNotificationChannel in MyService

--- app/src/main/java/com/example/whatsappandroid/MyService.java
+++ app/src/main/java/com/example/whatsappandroid/MyService.java
@@ -1,6 +1,12 @@
 package com.example.whatsappandroid;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+
 import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
@@ -12,6 +18,26 @@ public class MyService extends FirebaseMessagingService {
 
     @Override
     public void onMessageReceived(@NonNull RemoteMessage message) {
-        super.onMessageReceived(message);
+        if(message.getNotification()!=null){
+            createNotificationChannel();
+            NotificationCompat.Builder builder=new NotificationCompat.Builder(this,"1")
+                    .setSmallIcon(R.drawable.ic_launcher_foreground)
+                    .setContentTitle(message.getNotification().getTitle())
+                    .setContentText(message.getNotification().getBody())
+                    .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+            NotificationManagerCompat notificationManagerCompat=NotificationManagerCompat.from(this);
+            notificationManagerCompat.notify(1,builder.build());
+
+
+        }
+    }
+
+    private void createNotificationChannel(){
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.O){
+            int importance= NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel= new NotificationChannel("1","My channel",importance);
+            NotificationManager notificationManager=getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 }
